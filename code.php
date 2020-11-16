d831b3949393d81405611449b8bf50fae6c3c581
Ali R Kalantary <26902978+TheBiGBoSS6000@users.noreply.github.com>
2020-11-16 17:17:50 +0330

    Create code.php

---
+++ code.php
@@ -0,0 +1,22 @@
+<?php
+/* ------------------------ _____search_by_title_only */
+if ( ! function_exists('_____search_by_title_only') ) {
+
+  $post_type = array('product');
+
+	function _____product_search_by_title_only($where) {
+		global $wpdb, $typenow, $pagenow;
+		if ( ! is_admin() ) {
+			return $where;
+		}
+		if ( in_array($typenow, $post_type) && isset($_GET[ 's' ]) && 'edit.php' === $pagenow ) {
+			$terms = explode(' ', $_GET[ 's' ]);
+			foreach ( $terms as $term ) {
+				$where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql(like_escape($term)) . '%\'';
+			}
+			return $where;
+		}
+	}
+
+	add_filter('posts_search', '_____product_search_by_title_only', 500, 1);
+}
